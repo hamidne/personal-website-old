a48885563a3086b59fae9115c06128e1bb39e49a
HamidNE <hamidne@mail.ru>
2019-11-14 18:51:50 +0330

    add section 6 for index page

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
     <Section3 />
     <Section4 />
     <Section5 />
+    <Section6 />
   </div>
 </template>
 
@@ -14,7 +15,8 @@ import Section2 from "~/components/pages/index/Section2";
 import Section3 from "~/components/pages/index/Section3";
 import Section4 from "~/components/pages/index/Section4";
 import Section5 from "~/components/pages/index/Section5";
+import Section6 from "~/components/pages/index/Section6";
 export default {
-  components: { Section1, Section2, Section3, Section4, Section5 }
+  components: { Section1, Section2, Section3, Section4, Section5, Section6 }
 };
 </script>
