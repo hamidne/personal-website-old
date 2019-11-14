e600a63a575ca98ca13babd1f8edcff767fb46b0
HamidNE <hamidne@mail.ru>
2019-11-14 16:40:43 +0330

    fixed navbar contents

--- components/layouts/Navbar.vue
+++ components/layouts/Navbar.vue
@@ -1,53 +1,30 @@
 <template>
-  <nav
-    class="navbar navbar-expand-lg navbar-dark ftco_navbar ftco-navbar-light site-navbar-target"
-    id="ftco-navbar"
+  <b-navbar
+    toggleable="lg"
+    class="ftco_navbar ftco-navbar-light site-navbar-target"
   >
-    <div class="container">
-      <a class="navbar-brand" href="index.html"><span>R</span>onaldo</a>
-      <button
-        class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle"
-        type="button"
-        data-toggle="collapse"
-        data-target="#ftco-nav"
-        aria-controls="ftco-nav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="oi oi-menu"></span> Menu
-      </button>
+    <b-container>
+      <nuxt-link to="/" class="navbar-brand" title="Hamid Nasr">
+        <span>H</span>amid
+      </nuxt-link>
 
-      <div class="collapse navbar-collapse" id="ftco-nav">
-        <ul class="navbar-nav nav ml-auto">
-          <li class="nav-item">
-            <a href="#home-section" class="nav-link"><span>Home</span></a>
-          </li>
-          <li class="nav-item">
-            <a href="#about-section" class="nav-link"><span>About</span></a>
-          </li>
-          <li class="nav-item">
-            <a href="#resume-section" class="nav-link"><span>Resume</span></a>
-          </li>
-          <li class="nav-item">
-            <a href="#services-section" class="nav-link"
-              ><span>Services</span></a
-            >
-          </li>
-          <li class="nav-item">
-            <a href="#projects-section" class="nav-link"
-              ><span>Projects</span></a
-            >
-          </li>
-          <li class="nav-item">
-            <a href="#blog-section" class="nav-link"><span>My Blog</span></a>
-          </li>
-          <li class="nav-item">
-            <a href="#contact-section" class="nav-link"><span>Contact</span></a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+      <b-navbar-toggle target="nav-collapse">
+        <span class="oi oi-menu"></span> Menu
+      </b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="mr-auto text-white">
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">About</b-nav-item>
+          <b-nav-item href="#">Resume</b-nav-item>
+          <b-nav-item href="#">Services</b-nav-item>
+          <b-nav-item href="#">Projects</b-nav-item>
+          <b-nav-item href="#">My Blog</b-nav-item>
+          <b-nav-item href="#">Contact</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
 </template>
 
 <script>
