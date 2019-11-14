852856d8a99bb71020f3c65677a91a52150eec38
HamidNE <hamidne@mail.ru>
2019-11-14 21:35:23 +0330

    edit for section 4 contents

--- components/pages/index/Section4.vue
+++ components/pages/index/Section4.vue
@@ -8,7 +8,6 @@
               <li><a class="current" href="#page-1">تحصیلات</a></li>
               <li><a href="#page-2">تجربیات</a></li>
               <li><a href="#page-3">مهارت ها</a></li>
-              <li><a href="#page-4">دستاورد ها</a></li>
             </ul>
           </nav>
         </b-col>
@@ -153,119 +152,7 @@
             </div>
           </div>
           <div id="page-3" class="page three">
-            <h2 class="heading">Skills</h2>
-            <div class="row progress-circle mb-5">
-              <div class="col-lg-4 mb-4">
-                <div class="bg-white rounded-lg shadow p-4">
-                  <h2 class="h5 font-weight-bold text-center mb-4">CSS</h2>
-
-                  <!-- Progress bar 1 -->
-                  <div class="progress mx-auto" data-value="90">
-                    <span class="progress-left">
-                      <span class="progress-bar border-primary"></span>
-                    </span>
-                    <span class="progress-right">
-                      <span class="progress-bar border-primary"></span>
-                    </span>
-                    <div
-                      class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center"
-                    >
-                      <div class="h2 font-weight-bold">
-                        90<sup class="small">%</sup>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- END -->
-
-                  <!-- Demo info -->
-                  <div class="row text-center mt-4">
-                    <div class="col-6 border-right">
-                      <div class="h4 font-weight-bold mb-0">28%</div>
-                      <span class="small text-gray">Last week</span>
-                    </div>
-                    <div class="col-6">
-                      <div class="h4 font-weight-bold mb-0">60%</div>
-                      <span class="small text-gray">Last month</span>
-                    </div>
-                  </div>
-                  <!-- END -->
-                </div>
-              </div>
-
-              <div class="col-lg-4 mb-4">
-                <div class="bg-white rounded-lg shadow p-4">
-                  <h2 class="h5 font-weight-bold text-center mb-4">HTML</h2>
-
-                  <!-- Progress bar 1 -->
-                  <div class="progress mx-auto" data-value="80">
-                    <span class="progress-left">
-                      <span class="progress-bar border-primary"></span>
-                    </span>
-                    <span class="progress-right">
-                      <span class="progress-bar border-primary"></span>
-                    </span>
-                    <div
-                      class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center"
-                    >
-                      <div class="h2 font-weight-bold">
-                        80<sup class="small">%</sup>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- END -->
-
-                  <!-- Demo info -->
-                  <div class="row text-center mt-4">
-                    <div class="col-6 border-right">
-                      <div class="h4 font-weight-bold mb-0">28%</div>
-                      <span class="small text-gray">Last week</span>
-                    </div>
-                    <div class="col-6">
-                      <div class="h4 font-weight-bold mb-0">60%</div>
-                      <span class="small text-gray">Last month</span>
-                    </div>
-                  </div>
-                  <!-- END -->
-                </div>
-              </div>
-
-              <div class="col-lg-4 mb-4">
-                <div class="bg-white rounded-lg shadow p-4">
-                  <h2 class="h5 font-weight-bold text-center mb-4">jQuery</h2>
-
-                  <!-- Progress bar 1 -->
-                  <div class="progress mx-auto" data-value="75">
-                    <span class="progress-left">
-                      <span class="progress-bar border-primary"></span>
-                    </span>
-                    <span class="progress-right">
-                      <span class="progress-bar border-primary"></span>
-                    </span>
-                    <div
-                      class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center"
-                    >
-                      <div class="h2 font-weight-bold">
-                        75<sup class="small">%</sup>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- END -->
-
-                  <!-- Demo info -->
-                  <div class="row text-center mt-4">
-                    <div class="col-6 border-right">
-                      <div class="h4 font-weight-bold mb-0">28%</div>
-                      <span class="small text-gray">Last week</span>
-                    </div>
-                    <div class="col-6">
-                      <div class="h4 font-weight-bold mb-0">60%</div>
-                      <span class="small text-gray">Last month</span>
-                    </div>
-                  </div>
-                  <!-- END -->
-                </div>
-              </div>
-            </div>
+            <h2 class="heading">مهارت ها</h2>
             <div class="row">
               <div class="col-md-6 animate-box">
                 <div class="progress-wrap ">
@@ -368,77 +255,6 @@
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div id="page-4" class="page four">
-            <h2 class="heading">Awards</h2>
-            <div class="resume-wrap d-flex ">
-              <div
-                class="icon d-flex align-items-center justify-content-center"
-              >
-                <span class="flaticon-ideas"></span>
-              </div>
-              <div class="text pr-3">
-                <span class="date">2014-2015</span>
-                <h2>Top 10 Web Developer</h2>
-                <span class="position">Cambridge University</span>
-                <p>
-                  A small river named Duden flows by their place and supplies it
-                  with the necessary regelialia. It is a paradisematic country,
-                  in which roasted parts of sentences fly into your mouth.
-                </p>
-              </div>
-            </div>
-            <div class="resume-wrap d-flex ">
-              <div
-                class="icon d-flex align-items-center justify-content-center"
-              >
-                <span class="flaticon-ideas"></span>
-              </div>
-              <div class="text pr-3">
-                <span class="date">2014-2015</span>
-                <h2>Top 5 LeaderShip Exellence Winner</h2>
-                <span class="position">Cambridge University</span>
-                <p>
-                  A small river named Duden flows by their place and supplies it
-                  with the necessary regelialia. It is a paradisematic country,
-                  in which roasted parts of sentences fly into your mouth.
-                </p>
-              </div>
-            </div>
-            <div class="resume-wrap d-flex ">
-              <div
-                class="icon d-flex align-items-center justify-content-center"
-              >
-                <span class="flaticon-ideas"></span>
-              </div>
-              <div class="text pr-3">
-                <span class="date">2014-2015</span>
-                <h2>Top 4 Web Tester</h2>
-                <span class="position">Cambridge University</span>
-                <p>
-                  A small river named Duden flows by their place and supplies it
-                  with the necessary regelialia. It is a paradisematic country,
-                  in which roasted parts of sentences fly into your mouth.
-                </p>
-              </div>
-            </div>
-            <div class="resume-wrap d-flex ">
-              <div
-                class="icon d-flex align-items-center justify-content-center"
-              >
-                <span class="flaticon-ideas"></span>
-              </div>
-              <div class="text pr-3">
-                <span class="date">2014-2015</span>
-                <h2>Art &amp; Creative Director</h2>
-                <span class="position">Cambridge University</span>
-                <p>
-                  A small river named Duden flows by their place and supplies it
-                  with the necessary regelialia. It is a paradisematic country,
-                  in which roasted parts of sentences fly into your mouth.
-                </p>
               </div>
             </div>
           </div>
