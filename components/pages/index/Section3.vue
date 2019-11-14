5b0067aa18b2d4cb812801e8860863668214bf13
HamidNE <hamidne@mail.ru>
2019-11-14 17:24:55 +0330

    add section 3 and images

---
+++ components/pages/index/Section3.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="ftco-section ftco-partner">
+    <b-container>
+      <b-row>
+        <div class="col-sm ftco-animate">
+          <a href="#" class="partner"
+            ><img
+              src="~/assets/img/tools/partner-1.png"
+              class="img-fluid"
+              alt="Colorlib Template"
+          /></a>
+        </div>
+        <div class="col-sm ftco-animate">
+          <a href="#" class="partner"
+            ><img
+              src="~/assets/img/tools/partner-2.png"
+              class="img-fluid"
+              alt="Colorlib Template"
+          /></a>
+        </div>
+        <div class="col-sm ftco-animate">
+          <a href="#" class="partner"
+            ><img
+              src="~/assets/img/tools/partner-3.png"
+              class="img-fluid"
+              alt="Colorlib Template"
+          /></a>
+        </div>
+        <div class="col-sm ftco-animate">
+          <a href="#" class="partner"
+            ><img
+              src="~/assets/img/tools/partner-4.png"
+              class="img-fluid"
+              alt="Colorlib Template"
+          /></a>
+        </div>
+        <div class="col-sm ftco-animate">
+          <a href="#" class="partner"
+            ><img
+              src="~/assets/img/tools/partner-5.png"
+              class="img-fluid"
+              alt="Colorlib Template"
+          /></a>
+        </div>
+      </b-row>
+    </b-container>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,15 @@
   <div>
     <Section1 />
     <Section2 />
+    <Section3 />
   </div>
 </template>
 
 <script>
 import Section1 from "~/components/pages/index/Section1";
 import Section2 from "~/components/pages/index/Section2";
+import Section3 from "~/components/pages/index/Section3";
 export default {
-  components: { Section1, Section2 }
+  components: { Section1, Section2, Section3 }
 };
 </script>
