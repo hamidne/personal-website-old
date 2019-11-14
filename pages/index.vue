835c7c9132bcaac6c33c8d09f405da670bdcd6ad
HamidNE <hamidne@mail.ru>
2019-11-14 19:00:01 +0330

    add section 8 component for index page

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
     <Section5 />
     <Section6 />
     <Section7 />
+    <Section8 />
   </div>
 </template>
 
@@ -18,7 +19,8 @@ import Section4 from "~/components/pages/index/Section4";
 import Section5 from "~/components/pages/index/Section5";
 import Section6 from "~/components/pages/index/Section6";
 import Section7 from "~/components/pages/index/Section7";
+import Section8 from "~/components/pages/index/Section8";
 export default {
-  components: { Section1, Section2, Section3, Section4, Section5, Section6, Section7 }
+  components: { Section1, Section2, Section3, Section4, Section5, Section6, Section7, Section8 }
 };
 </script>
