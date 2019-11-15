282e1cde43eb456e81580eae151c430e7ef10c2e
HamidNE <hamidne@mail.ru>
2019-11-15 06:49:07 +0330

    add new blog section for index page

---
+++ components/pages/index/BlogSection.vue
@@ -0,0 +1,114 @@
+<template>
+  <section class="ftco-section" id="blog-section">
+    <div class="container">
+      <div class="row justify-content-center mb-5 pb-5">
+        <div class="col-md-7 heading-section text-center">
+          <h1 class="big big-2">Blog</h1>
+          <h2 class="mb-4">Our Blog</h2>
+          <p>
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia
+          </p>
+        </div>
+      </div>
+      <div class="row d-flex">
+        <div class="col-md-4 d-flex">
+          <div class="blog-entry justify-content-end">
+            <a
+              href="single.html"
+              class="block-20"
+              style="background-image: url('https://picsum.photos/800/600?7');"
+            >
+            </a>
+            <div class="text mt-3 float-right d-block">
+              <h3 class="heading">
+                <a href="single.html"
+                  >Why Lead Generation is Key for Business Growth</a
+                >
+              </h3>
+              <div class="d-flex align-items-center mb-3 meta">
+                <p class="mb-0">
+                  <span class="mr-2">Sept. 12, 2019</span>
+                  <a href="#" class="mr-2">Admin</a>
+                  <a href="#" class="meta-chat"
+                    ><span class="icon-chat"></span> 3</a
+                  >
+                </p>
+              </div>
+              <p>
+                A small river named Duden flows by their place and supplies it
+                with the necessary regelialia.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 d-flex">
+          <div class="blog-entry justify-content-end">
+            <a
+              href="single.html"
+              class="block-20"
+              style="background-image: url('https://picsum.photos/800/600?8');"
+            >
+            </a>
+            <div class="text mt-3 float-right d-block">
+              <h3 class="heading">
+                <a href="single.html"
+                  >Why Lead Generation is Key for Business Growth</a
+                >
+              </h3>
+              <div class="d-flex align-items-center mb-3 meta">
+                <p class="mb-0">
+                  <span class="mr-2">Sept. 12, 2019</span>
+                  <a href="#" class="mr-2">Admin</a>
+                  <a href="#" class="meta-chat"
+                    ><span class="icon-chat"></span> 3</a
+                  >
+                </p>
+              </div>
+              <p>
+                A small river named Duden flows by their place and supplies it
+                with the necessary regelialia.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 d-flex">
+          <div class="blog-entry">
+            <a
+              href="single.html"
+              class="block-20"
+              style="background-image: url('https://picsum.photos/800/600?9');"
+            >
+            </a>
+            <div class="text mt-3 float-right d-block">
+              <h3 class="heading">
+                <a href="single.html"
+                  >Why Lead Generation is Key for Business Growth</a
+                >
+              </h3>
+              <div class="d-flex align-items-center mb-3 meta">
+                <p class="mb-0">
+                  <span class="mr-2">Sept. 12, 2019</span>
+                  <a href="#" class="mr-2">Admin</a>
+                  <a href="#" class="meta-chat"
+                    ><span class="icon-chat"></span> 3</a
+                  >
+                </p>
+              </div>
+              <p>
+                A small river named Duden flows by their place and supplies it
+                with the necessary regelialia.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <about-section />
     <resume-section />
     <project-section />
+    <blog-section />
     <Section7 />
     <contact-section />
   </div>
@@ -11,15 +12,18 @@
 
 <script>
 import Section7 from "~/components/pages/index/Section7";
+import BlogSection from "~/components/pages/index/BlogSection";
 import AboutSection from "~/components/pages/index/AboutSection";
 import ResumeSection from "~/components/pages/index/ResumeSection";
 import HeaderSection from "~/components/pages/index/HeaderSection";
 import ProjectSection from "~/components/pages/index/ProjectSection";
 import CounterSection from "~/components/pages/index/CounterSection";
 import ContactSection from "~/components/pages/index/ContactSection";
+
 export default {
   components: {
     Section7,
+    BlogSection,
     AboutSection,
     HeaderSection,
     ResumeSection,
