09915e07843ba0e6b9d609849f9415103b0624dc
HamidNE <hamidne@mail.ru>
2019-11-15 08:42:29 +0330

    edit for project section contents

--- components/pages/index/ProjectSection.vue
+++ components/pages/index/ProjectSection.vue
@@ -1,88 +1,26 @@
 <template>
   <section class="ftco-section ftco-project" id="projects-section">
     <div class="container-fluid px-md-0">
-      <div class="row no-gutters justify-content-center pb-5">
-        <div class="col-md-12 heading-section text-center ">
+      <b-row class="no-gutters justify-content-center pb-5">
+        <b-col md="12" class="heading-section text-center ">
           <h1 class="big big-2">پروژه ها</h1>
           <h2 class="mb-4">پروژه های من</h2>
           <p>
             Far far away, behind the word mountains, far from the countries
             Vokalia and Consonantia
           </p>
-        </div>
-      </div>
+        </b-col>
+      </b-row>
       <div class="row no-gutters">
-        <div class="col-md-4">
+        <div class="col-md-4" v-for="(project, index) in projects" :key="index">
           <div
-            id="cafimafi-project"
+            :id="`${project.slug}-project`"
             class="project img  d-flex justify-content-center align-items-center"
           >
             <div class="overlay"></div>
             <div class="text text-center p-4">
-              <h3><a href="#">پروژه فروشگاه قهوه کافی مافی</a></h3>
-              <span>Web Design</span> |
-              <span>Web Design</span> |
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div
-            id="tarhan-project"
-            class="project img  d-flex justify-content-center align-items-center"
-          >
-            <div class="overlay"></div>
-            <div class="text text-center p-4">
-              <h3><a href="#">Branding &amp; Illustration Design</a></h3>
-              <span>Web Design</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div
-            id="gerama-project"
-            class="project img  d-flex justify-content-center align-items-center"
-          >
-            <div class="overlay"></div>
-            <div class="text text-center p-4">
-              <h3><a href="#">Branding &amp; Illustration Design</a></h3>
-              <span>Web Design</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div
-            id="irangfx-project"
-            class="project img  d-flex justify-content-center align-items-center"
-          >
-            <div class="overlay"></div>
-            <div class="text text-center p-4">
-              <h3><a href="#">Branding &amp; Illustration Design</a></h3>
-              <span>Web Design</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div
-            id="taakproject-project"
-            class="project img  d-flex justify-content-center align-items-center"
-          >
-            <div class="overlay"></div>
-            <div class="text text-center p-4">
-              <h3><a href="#">Branding &amp; Illustration Design</a></h3>
-              <span>Web Design</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div
-            id="vuejsconf-project"
-            class="project img  d-flex justify-content-center align-items-center"
-          >
-            <div class="overlay"></div>
-            <div class="text text-center p-4">
-              <h3><a href="#">Branding &amp; Illustration Design</a></h3>
-              <span>Web Design</span>
+              <h3><a :href="project.link" v-text="project.title" target="blank" /></h3>
+              <span>Web Design</span> | <span>Web Design</span> |
             </div>
           </div>
         </div>
@@ -92,7 +30,14 @@
 </template>
 
 <script>
-export default {};
+import projects from "~/data/projects";
+export default {
+  data() {
+    return {
+      projects
+    };
+  }
+};
 </script>
 
 <style>
---
+++ data/projects.js
@@ -0,0 +1,8 @@
+export default [
+  { slug: "cafimafi", title: "پروژه فروشگاه قهوه کافی مافی", link: 'https://cafimafi.com' },
+  { slug: "tarhan", title: "پلتفرم طراحی و چاپ آنلاین", link: 'https://www.tarhan.ir' },
+  { slug: "gerama", title: "اپلیکیشن موسیقی بی کلام", link: 'https://gerama.ir' },
+  { slug: "irangfx", title: "سایت دانلود ابزار طراحان گرافیک", link: 'https://irangfx.com' },
+  { slug: "taakproject", title: "پروژه دانش آزاد تاک", link: 'https://taakproject.ir' },
+  { slug: "vuejsconf", title: "سایت جامعه vue.js ایران", link: 'https://vuejsconf.ir' }
+];
