f0c83a5d383de53f9b48fc461e355fb949ec8c91
HamidNE <hamidne@mail.ru>
2019-11-15 08:11:35 +0330

    edit for blog section contents

--- components/pages/index/BlogSection.vue
+++ components/pages/index/BlogSection.vue
@@ -1,109 +1,48 @@
 <template>
   <section class="ftco-section" id="blog-section">
-    <div class="container">
-      <div class="row justify-content-center mb-5 pb-5">
-        <div class="col-md-7 heading-section text-center">
-          <h1 class="big big-2">Blog</h1>
-          <h2 class="mb-4">Our Blog</h2>
+    <b-container>
+      <b-row class="justify-content-center mb-5 pb-5">
+        <b-col md="7" class="heading-section text-center">
+          <h1 class="big big-2">بلاگ</h1>
+          <h2 class="mb-4">وبلاگ من</h2>
           <p>
             Far far away, behind the word mountains, far from the countries
             Vokalia and Consonantia
           </p>
-        </div>
-      </div>
-      <div class="row d-flex">
-        <div class="col-md-4 d-flex">
-          <div class="blog-entry justify-content-end">
-            <a
-              href="single.html"
-              class="block-20"
-              style="background-image: url('https://picsum.photos/800/600?7');"
-            >
-            </a>
-            <div class="text mt-3 float-right d-block">
-              <h3 class="heading">
-                <a href="single.html"
-                  >Why Lead Generation is Key for Business Growth</a
-                >
-              </h3>
-              <div class="d-flex align-items-center mb-3 meta">
-                <p class="mb-0">
-                  <span class="mr-2">Sept. 12, 2019</span>
-                  <a href="#" class="mr-2">Admin</a>
-                  <a href="#" class="meta-chat"
-                    ><span class="icon-chat"></span> 3</a
-                  >
-                </p>
-              </div>
-              <p>
-                A small river named Duden flows by their place and supplies it
-                with the necessary regelialia.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 d-flex">
-          <div class="blog-entry justify-content-end">
-            <a
-              href="single.html"
-              class="block-20"
-              style="background-image: url('https://picsum.photos/800/600?8');"
-            >
-            </a>
-            <div class="text mt-3 float-right d-block">
-              <h3 class="heading">
-                <a href="single.html"
-                  >Why Lead Generation is Key for Business Growth</a
-                >
-              </h3>
-              <div class="d-flex align-items-center mb-3 meta">
-                <p class="mb-0">
-                  <span class="mr-2">Sept. 12, 2019</span>
-                  <a href="#" class="mr-2">Admin</a>
-                  <a href="#" class="meta-chat"
-                    ><span class="icon-chat"></span> 3</a
-                  >
-                </p>
-              </div>
-              <p>
-                A small river named Duden flows by their place and supplies it
-                with the necessary regelialia.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 d-flex">
-          <div class="blog-entry">
-            <a
-              href="single.html"
-              class="block-20"
-              style="background-image: url('https://picsum.photos/800/600?9');"
-            >
-            </a>
-            <div class="text mt-3 float-right d-block">
-              <h3 class="heading">
-                <a href="single.html"
-                  >Why Lead Generation is Key for Business Growth</a
-                >
-              </h3>
-              <div class="d-flex align-items-center mb-3 meta">
-                <p class="mb-0">
-                  <span class="mr-2">Sept. 12, 2019</span>
-                  <a href="#" class="mr-2">Admin</a>
-                  <a href="#" class="meta-chat"
-                    ><span class="icon-chat"></span> 3</a
-                  >
-                </p>
-              </div>
-              <p>
-                A small river named Duden flows by their place and supplies it
-                with the necessary regelialia.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+        </b-col>
+      </b-row>
+      <b-row class="row d-flex">
+        <b-col md="4">
+          <b-card
+            no-body
+            img-src="https://jadi.net/wp-content/uploads/2019/11/wordsmith-768x402.png"
+            img-alt="Image"
+            tag="article"
+            class="shadow-sm border-0"
+          >
+            <b-card-body>
+              <b-card-title>
+                خبرساز هوش مصنوعی‌ای که گفته بودیم برای انتشار بیش از حد
+                خطرناکه، منتشر شد
+              </b-card-title>
+              <b-card-sub-title class="mb-2">هوش مصنوعی</b-card-sub-title>
+              <b-card-text>
+                خبرساز هوش مصنوعی‌ای که گفته بودیم برای انتشار بیش از حد
+                خطرناکه، منتشر شد مدتی قبل توی رادیو گیک از یک سیستم هوش مصنوعی
+                گفته بودیم که سازندگانش می گفتن برای انتشار بیش از حد خطرناکه و
+                اونو فعلا منتشر نیم کنن. این سیستم می‌تونست با گرفتن یه تیتر یا
+                چند جمله اول یک خبر، براش ادامه ای رو جعل کنه که از نظر ما
+                انسان‌ها، منطقی و طبیعی به نظر برسه. حالا OpenAI این برنامه رو
+                منتشر کرده.
+              </b-card-text>
+              <b-button href="https://jadi.net/2019/11/ai-gpt2/" variant="primary" block>
+                مشاهده
+              </b-button>
+            </b-card-body>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </section>
 </template>
 
