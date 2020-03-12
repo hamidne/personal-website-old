0c063ef03de74176999b4e09671393fbf9170066
HamidNE <hamidne@mail.ru>
2020-03-12 21:33:34 +0330

    add multi lang config

---
+++ config/i18n.js
@@ -0,0 +1,52 @@
+export default {
+  locales: ["fa", "en", "ar"],
+  defaultLocale: "fa",
+  lazy: true,
+  langDir: "lang/",
+  locales: [
+    {
+      code: "fa",
+      file: "fa-IR.js"
+    },
+    {
+      code: "en",
+      file: "en-US.js"
+    },
+    {
+      code: "ar",
+      file: "ar-AS.js"
+    }
+  ],
+  projects: [
+    {
+      slug: "cafimafi",
+      title: "پروژه فروشگاه قهوه کافی مافی",
+      link: "https://cafimafi.com"
+    },
+    {
+      slug: "tarhan",
+      title: "پلتفرم طراحی و چاپ آنلاین",
+      link: "https://www.tarhan.ir"
+    },
+    {
+      slug: "gerama",
+      title: "اپلیکیشن موسیقی بی کلام",
+      link: "https://gerama.ir"
+    },
+    {
+      slug: "irangfx",
+      title: "سایت دانلود ابزار طراحان گرافیک",
+      link: "https://irangfx.com"
+    },
+    {
+      slug: "taakproject",
+      title: "پروژه دانش آزاد تاک",
+      link: "https://taakproject.ir"
+    },
+    {
+      slug: "vuejsconf",
+      title: "سایت جامعه vue.js ایران",
+      link: "https://vuejsconf.ir"
+    }
+  ]
+};
--- config/modules.js
+++ config/modules.js
@@ -1,5 +1,6 @@
 export default [
   // "@nuxtjs/pwa",
+  "nuxt-i18n",
   "bootstrap-vue/nuxt",
   ["vue-scrollto/nuxt", { duration: 1000 }]
 ];
